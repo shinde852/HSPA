65b8454e8636a92c19782d35e7f490464008b2ef
govind <shindegovind001@gmail.com>
2021-03-28 21:43:40 +0530

    test

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyListComponent,
+    NavBarComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
